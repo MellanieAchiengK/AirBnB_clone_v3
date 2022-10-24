8c253bab9cb9ec7238241904f12fe38ebf733982
mellanie <mellanie.achieng@gmail.com>
2022-10-24 20:03:36 +0000

    merge fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -10,18 +9,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-=======
-#!/usr/bin/python3
-""" init file """
-
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.places_reviews import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.places import *
->>>>>>> c02c8bf79a11e249678224b436b61ec738225fff
--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,69 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-'''contains amenity routes'''
-from flask import Flask, jsonify, abort, request
-from api.v1.views import app_views
-from models import storage
-from models.amenity import Amenity
-
-
-@app_views.route('/amenities', methods=['GET'])
-def get_amenities():
-    '''retrieves amenity objects'''
-    amenity_list = [a.to_dict() for a in storage.all(Amenity).values()]
-    return jsonify(amenity_list)
-
-
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['GET'])
-def get_amenity_id(amenity_id):
-    '''returns amenity with given id'''
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    return jsonify(amenity.to_dict())
-
-
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['DELETE'])
-def delete_amenity(amenity_id):
-    '''deletes amenity object using amenity_id'''
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
-        abort(404)
-    amenity.delete()
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/amenities', methods=['POST'])
-def create_amenities():
-    '''create new amenity obj'''
-    if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
-    elif 'name' not in request.get_json():
-        return jsonify({'error': 'Missing name'}), 400
-    else:
-        obj_data = request.get_json()
-        obj = Amenity(**obj_data)
-        obj.save()
-        return jsonify(obj.to_dict()), 201
-
-
-@app_views.route('/amenities/<amenities_id>',
-                 methods=['PUT'])
-def update_amenity(amenities_id):
-    '''update existing amenity object'''
-    if not request.get_json():
-        return jsonify({'error': 'Not a JSON'}), 400
-    obj = storage.get(Amenity, amenities_id)
-    if obj is None:
-        abort(404)
-    obj_data = request.get_json()
-    obj.name = obj_data['name']
-    obj.save()
-    return jsonify(obj.to_dict()), 200
-=======
 """Amenities"""
 
 from api.v1.views import app_views
@@ -140,4 +75,3 @@ def updateamenity(amenity_id=None):
         storage.save()
         res = obj.to_dict()
         return jsonify(res), 200
->>>>>>> c02c8bf79a11e249678224b436b61ec738225fff
